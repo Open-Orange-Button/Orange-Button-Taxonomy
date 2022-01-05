162caa805b6b0712868b3370b6b1b1413e13effe
reepoi <spacechips@rocketmail.com>
2022-01-05 13:08:12 -0700

    moving spellcheck regex into .spectral.yaml

--- .github/configs/schema-validator/functions/spellcheckSubstrings.js
+++ .github/configs/schema-validator/functions/spellcheckSubstrings.js
@@ -3,8 +3,7 @@ let dictionaries;
 export default (input, options) => {
   // initialize dictionaries (global) on first call to function only to prevent initialization on every function call
   dictionaries = dictionaries || options.dictionaries.map(dictInfo => new Typo(dictInfo.name, false, false, { dictionaryPath: dictInfo.path }));
-  // split input into substrings of: numbers, one capital followed by lowercase letters, all-caps letters
-  let substrings = input.match(/[0-9]+|[A-Z][a-z]+|[A-Z]+(?![a-z])/g) || [];
+  let substrings = input.match(new RegExp(options.substringMatch, 'g')) || [];
   // filter out numbers since numbers are not words
   let nonNumericalSubstrings = substrings.filter(isNaN);
   // filter for substrings not in any dictionary
