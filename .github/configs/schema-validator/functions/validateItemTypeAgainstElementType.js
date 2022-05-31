7f22519b9b94f12392637b3881e5fde2ef047ceb
reepoi <spacechips@rocketmail.com>
2022-05-31 08:24:40 -0700

    adding check for nonnumeric OpenAPI Types paired with unit Item Types

--- .github/configs/schema-validator/functions/validateItemTypeAgainstElementType.js
+++ .github/configs/schema-validator/functions/validateItemTypeAgainstElementType.js
@@ -1,13 +1,15 @@
 const numericOpenAPITypes = ['number', 'integer'];
+const nonnumericOpenAPITypes = ['boolean', 'string'];
 export default (input, options, context) => {
     let taxonomy = context.document.data;
     let defValue = input[0].properties.Value;
     let isArray = defValue.type === 'array';
-    console.warn(defValue)
     let elementType = isArray ? defValue.items.type : defValue.type;
     let itemType = input[1]['x-ob-item-type'];
     let itemTypeDef = taxonomy['x-ob-item-types'][itemType];
     if (numericOpenAPITypes.includes(elementType) && itemTypeDef.enums) {
-        return [{ message: `This element's Value primitive ${isArray ? 'is an' : 'has items of'} OpenAPI type ${elementType}, but the item type '${itemType}' defines string type enumerations.` }];
+        return [{ message: `This element's Value primitive ${isArray ? 'has items of' : 'is an'} OpenAPI type ${elementType}, but the item type '${itemType}' defines string type enumerations.` }];
+    } else if (nonnumericOpenAPITypes.includes(elementType) && itemTypeDef.units) {
+        return [{ message: `This element's Value primitive ${isArray ? 'has items of' : 'is an'} OpenAPI type ${elementType}, but the item type '${itemType}' defines units.` }];
     }
 }
