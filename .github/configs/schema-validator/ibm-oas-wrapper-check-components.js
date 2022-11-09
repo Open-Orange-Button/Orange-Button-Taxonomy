a59bdfbda3d04334abdd2f2ce075845a3b5c5ce9
Taos Transue <41020789+reepoi@users.noreply.github.com>
2022-11-09 08:07:12 -0800

    fixing linter jsonpath-plus query for OB element (#134)

--- .github/configs/schema-validator/ibm-oas-wrapper-check-components.js
+++ .github/configs/schema-validator/ibm-oas-wrapper-check-components.js
@@ -1,6 +1,6 @@
 /**
  * Wrapper of IBM OAS (@ibm-cloud/openapi-ruleset) to let its schema rules check schema definitions in OpenAPI components.
- * 
+ *
  * The code here (https://github.com/IBM/openapi-validator/blob/098138ab7387a18f1a600ff33da9430ed1c4461f/packages/ruleset/src/collections/index.js#L11)
  * shows that schema rules of IBM OAS only check schema definitions that are used in an OpenAPI path, not those only defined in OpenAPI components.
  */
