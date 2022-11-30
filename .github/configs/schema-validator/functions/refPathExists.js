db2806b2a1dfa124da3de5172437faabd0938a72
Cliff Hansen <cwhanse@sandia.gov>
2022-11-30 11:07:53 -0700

    remove gh actions

--- .github/configs/schema-validator/functions/refPathExists.js
+++
@@ -1,7 +0,0 @@
-export default (input, options, context) => {
-  let refPathResolvedObj = input.split('/').filter(key => key !== '#')
-    .reduce((obj, key) => obj[key] ? obj[key] : false, context.document.data);
-  if (!refPathResolvedObj) {
-    return [{ message: input }];
-  }
-}
--- .github/configs/schema-validator/functions/validateXOBFields.js
+++
@@ -1,6 +0,0 @@
-export default (input, options, context) => {
-  let items = Object.keys(context.document.data[options.objectPath]);
-  if (input && !items.includes(input)) {
-    return [{ message: input }];
-  }
-}
